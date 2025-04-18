79e610face49e0fc3f5e768f34f8f98ff1ee47de
Gulf1603 <hugoleh@icloud.com>
2025-04-18 16:14:29 +0200

    Small fix on the cast_ray.

--- srcs/game/draw/raycast/draw_map.c
+++ srcs/game/draw/raycast/draw_map.c
@@ -6,46 +6,11 @@
 /*   By: hle-hena <hle-hena@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/13 22:06:06 by hle-hena          #+#    #+#             */
-/*   Updated: 2025/04/18 16:09:13 by hle-hena         ###   ########.fr       */
+/*   Updated: 2025/04/18 16:14:05 by hle-hena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
-
-void draw_wall(t_data *data, t_hit ray_hit, t_point limit, int x)
-{
-	int		y;
-	int		tex_x;
-	int		tex_y;
-	float	wall_x;
-	char	*row;
-
-	if (ray_hit.side == 0)
-		wall_x = ray_hit.ray_hit.y;
-	else
-		wall_x = ray_hit.ray_hit.x;
-	wall_x -= (int)wall_x;
-	tex_x = (int)(wall_x * (float)ray_hit.texture.width);
-	if (tex_x < 0)
-		tex_x = 0;
-	if (tex_x >= ray_hit.texture.width)
-		tex_x = ray_hit.texture.width - 1;
-	y = ft_max(limit.x - 1, 0);
-	row = data->img.data + y * data->img.size_line + x * data->img.bpp;
-	int line_height = limit.y - limit.x;
-	int	draw_end = ft_min(limit.y, data->win_len);
-	int step_fp = (ray_hit.texture.height << 16) / line_height;
-	int tex_pos_fp = ((ft_max(limit.x, 0) - data->win_len / 2 + line_height / 2) * step_fp);
-	char *tex_col = ray_hit.texture.data + tex_x * ray_hit.texture.bpp;
-	int tex_stride = ray_hit.texture.size_line;
-	while (++y < draw_end)
-	{
-		tex_y = tex_pos_fp >> 16;
-		*(int *)row = *(int *)(tex_col + tex_y * tex_stride);
-		row += data->img.size_line;
-		tex_pos_fp += step_fp;
-	}
-}
 
 	// struct timespec start, end;
 	// clock_gettime(CLOCK_MONOTONIC, &start);
@@ -85,14 +50,15 @@ t_hit cast_ray(t_data *data, t_vec ray_dir)
 	int		line_height;
 	int		tex_start;
 	int		tex_end;
-	int		tex_x;
 
 	ray_hit = raycast(data, ray_dir, data->map->player);
 	ray_hit.ray_dir = ray_dir;
-	ray_hit.dist = ray_hit.dist * (ray_dir.x * data->cam.dir.x + ray_dir.y * data->cam.dir.y);;
+	ray_hit.dist = ray_hit.dist * (ray_dir.x * data->cam.dir.x + ray_dir.y * data->cam.dir.y);
 	if (ray_hit.dist <= 0.0f)
 		ray_hit.dist = 0.0001f;
 	line_height = (int)(data->win_len / ray_hit.dist);
+	if (line_height == 0)
+		line_height = 1;
 	tex_start = -line_height / 2 + data->win_len / 2;
 	tex_end = line_height / 2 + data->win_len / 2;
 	ray_hit.draw_start = ft_max(tex_start, 0);
@@ -102,11 +68,10 @@ t_hit cast_ray(t_data *data, t_vec ray_dir)
 	else
 		wall_x = ray_hit.ray_hit.x;
 	wall_x -= (int)wall_x;
-	tex_x = (int)(wall_x * ray_hit.texture.width);
 	line_height = tex_end - tex_start;
 	ray_hit.step_fp = (ray_hit.texture.height << 16) / line_height;
 	ray_hit.tex_pos_fp = (ray_hit.draw_start - data->win_len / 2 + line_height / 2) * ray_hit.step_fp;
-	ray_hit.tex_col = ray_hit.texture.data + tex_x * ray_hit.texture.bpp;
+	ray_hit.tex_col = ray_hit.texture.data + (int)(wall_x * ray_hit.texture.width) * ray_hit.texture.bpp;
 	return (ray_hit);
 }
 
