0960f37b0bdabeb1abb1b15cf07a7e5a2cd27035
Gulf1603 <hugoleh@icloud.com>
2025-06-06 17:37:11 +0200

    Changed to fixed point, however, the performance is still quite bad. I need to find a solution.

--- srcs/game/draw/raycast/draw_map.c
+++ srcs/game/draw/raycast/draw_map.c
@@ -6,7 +6,7 @@
 /*   By: hle-hena <hle-hena@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/13 22:06:06 by hle-hena          #+#    #+#             */
-/*   Updated: 2025/06/06 14:16:59 by hle-hena         ###   ########.fr       */
+/*   Updated: 2025/06/06 17:34:54 by hle-hena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,7 +44,7 @@ static inline t_vec reflect_across_mirror(t_vec point, t_hit *hit, t_point curr,
 }
 
 
-static inline int	color_blend(int base_color, int light_color, float emittance)
+static inline t_col	color_blend(int base_color, int light_color, float emittance)
 {
 	int		em;
 	t_col	base;
@@ -52,7 +52,7 @@ static inline int	color_blend(int base_color, int light_color, float emittance)
 	t_col	final;
 
 	if (emittance < 0.001)
-		return (0);
+		return ((t_col){0});
 	em = (int)(emittance * 256.0f);
 	base = (t_col){(base_color >> 16) & 0xFF,
 		(base_color >> 8) & 0xFF,
@@ -64,7 +64,7 @@ static inline int	color_blend(int base_color, int light_color, float emittance)
 		(base.gr * ((light.gr * em) >> 8)) >> 8,
 		(base.bl * ((light.bl * em) >> 8)) >> 8,
 	};
-	return ((final.re << 16) | (final.gr << 8) | final.bl);
+	return (final);
 }
 
 static inline void	set_pixels(t_data *data, char *img, int color)
@@ -75,62 +75,92 @@ static inline void	set_pixels(t_data *data, char *img, int color)
 	*(int *)(img + data->img.size_line + data->img.bpp) = color;
 }
 
-static inline float	to_linear(float c)
+#define FP_SHIFT 16
+#define FP_ONE (1 << FP_SHIFT)
+#define TO_FP(x) ((int)((x) * (float)(1 << FP_SHIFT) + 0.5f))
+#define FROM_FP(x) ((float)x / FP_ONE)
+#define MUL_FP(a, b) (((long long)(a) * (long long)(b)) >> FP_SHIFT)
+#define DIV_FP(a, b) (((long long)(a) << FP_SHIFT) / (long long)(b))
+
+static inline int to_linear(int c)
 {
-	return ((c / 255.0f) * (c / 255.0f));
+	int c_fp = DIV_FP(TO_FP(c), TO_FP(255.0f));
+	return MUL_FP(c_fp, c_fp);
 }
 
-static inline float	to_srgb(float c)
+#define SRGB_TABLE_SIZE 1024
+static short sqrt_fp_to_srgb[SRGB_TABLE_SIZE];
+
+void init_sqrt_fp_to_srgb(void)
 {
-	return (sqrtf(c) * 255.0f);
+	for (int i = 0; i < SRGB_TABLE_SIZE; ++i)
+	{
+		float x = (float)i / (SRGB_TABLE_SIZE - 1);
+		sqrt_fp_to_srgb[i] = (short)(sqrtf(x) * 255.0f + 0.5f);
+	}
 }
 
-static inline int blend_color(int base_col, int reflected_col, float reflectance)
+static inline int to_srgb(int fp_val)
 {
-	float inv = powf(1.0f - reflectance, 2.2f);
-	reflectance = 1.0f - inv;
-	float r1 = to_linear((base_col >> 16) & 0xFF);
-	float g1 = to_linear((base_col >> 8) & 0xFF);
-	float b1 = to_linear(base_col & 0xFF);
-	float r2 = to_linear((reflected_col >> 16) & 0xFF);
-	float g2 = to_linear((reflected_col >> 8) & 0xFF);
-	float b2 = to_linear(reflected_col & 0xFF);
-	float r = r1 * (1.0f - reflectance) + (r1 * r2) * reflectance;
-	float g = g1 * (1.0f - reflectance) + (g1 * g2) * reflectance;
-	float b = b1 * (1.0f - reflectance) + (b1 * b2) * reflectance;
-	int ri = (int)to_srgb(r);
-	int gi = (int)to_srgb(g);
-	int bi = (int)to_srgb(b);
-	if (ri > 255) ri = 255;
-	if (gi > 255) gi = 255;
-	if (bi > 255) bi = 255;
-	return (ri << 16) | (gi << 8) | bi;
+	if (fp_val < 0)
+		fp_val = 0;
+	else if (fp_val >= FP_ONE)
+		fp_val = FP_ONE - 1;
+	int idx = (int)(((long long)fp_val * (SRGB_TABLE_SIZE - 1)) >> FP_SHIFT);
+	return sqrt_fp_to_srgb[idx];
 }
 
+static inline t_col blend_color(t_col base_col, t_col reflected_col, int reflectance_fp)
+{
+	int inv_reflectance = (FP_ONE - reflectance_fp);
+	inv_reflectance = MUL_FP(inv_reflectance, inv_reflectance);
+	reflectance_fp = FP_ONE - inv_reflectance;
+	int r1 = to_linear(base_col.re);
+	int g1 = to_linear(base_col.gr);
+	int b1 = to_linear(base_col.bl);
+	int r2 = to_linear(reflected_col.re);
+	int g2 = to_linear(reflected_col.gr);
+	int b2 = to_linear(reflected_col.bl);
+	int r = MUL_FP(r1, inv_reflectance) + MUL_FP(MUL_FP(r1, r2), reflectance_fp);
+	int g = MUL_FP(g1, inv_reflectance) + MUL_FP(MUL_FP(g1, g2), reflectance_fp);
+	int b = MUL_FP(b1, inv_reflectance) + MUL_FP(MUL_FP(b1, b2), reflectance_fp);
+	t_col final = {
+		to_srgb(r),
+		to_srgb(g),
+		to_srgb(b)
+	};
+	if (final.re > 255)
+		final.re = 255;
+	if (final.gr > 255)
+		final.gr = 255;
+	if (final.bl > 255)
+		final.bl = 255;
+	return (final);
+}
 
-static inline float	demenish_light(t_wpath *walls, float base_emittance, int max)
+static inline int	demenish_light(t_wpath *walls, int base_emittance, int max)
 {
 	int	i;
 
 	i = -1;
 	while (++i < max)
-		base_emittance *= walls[i].reflectance;
+		base_emittance = MUL_FP(base_emittance, TO_FP(walls[i].reflectance));
 	return (base_emittance);
 }
 
-static inline int	get_color(t_hit *hit, int nb_hit, int fall_back_color, int isnt_wall)
+static inline int	get_color(t_hit *hit, int nb_hit, t_col fall_back_color, int isnt_wall)
 {
-	int	final_color;
-	int	prev_wall_color;
-	int	wall_color;
+	t_col	final_color;
+	t_col	prev_wall_color;
+	t_col	wall_color;
 
 	if (isnt_wall)
 		prev_wall_color = fall_back_color;
 	else
-	{	
+	{
 		prev_wall_color = color_blend(*(int *)(hit->tex_col[nb_hit]
 			+ (hit->tex_pos_fp[nb_hit] >> 16) * hit->texture[nb_hit]->size_line),
-			hit->light[nb_hit]->color, demenish_light(hit->wall, hit->light[nb_hit]->emittance, nb_hit));
+			hit->light[nb_hit]->color, FROM_FP(demenish_light(hit->wall, TO_FP(hit->light[nb_hit]->emittance), nb_hit)));
 		hit->tex_pos_fp[nb_hit] += hit->step_fp[nb_hit];
 	}
 	final_color = prev_wall_color;
@@ -138,13 +168,13 @@ static inline int	get_color(t_hit *hit, int nb_hit, int fall_back_color, int isn
 	{
 		wall_color = color_blend(*(int *)(hit->tex_col[nb_hit]
 			+ (hit->tex_pos_fp[nb_hit] >> 16) * hit->texture[nb_hit]->size_line),
-			hit->light[nb_hit]->color, demenish_light(hit->wall, hit->light[nb_hit]->emittance, nb_hit));
+			hit->light[nb_hit]->color, FROM_FP(demenish_light(hit->wall, TO_FP(hit->light[nb_hit]->emittance), nb_hit)));
 		final_color = blend_color(wall_color, prev_wall_color,
-			hit->wall[nb_hit].reflectance);
+			TO_FP(hit->wall[nb_hit].reflectance));
 		prev_wall_color = final_color;
 		hit->tex_pos_fp[nb_hit] += hit->step_fp[nb_hit];
 	}
-	return (final_color);
+	return ((final_color.re << 16) | (final_color.gr << 8) | final_color.bl);
 }
 
 static inline void	draw_ceil(t_data *data, t_point curr, t_rdir ray, char *img, t_hit *hit)
@@ -176,7 +206,7 @@ static inline void	draw_ceil(t_data *data, t_point curr, t_rdir ray, char *img, 
 	light = data->lmap.lmap[(int)(world.x * LMAP_PRECISION)
 		+ (int)(world.y * LMAP_PRECISION) * data->lmap.wid].ce_fl;
 	set_pixels(data, img, get_color(hit, bounce - 1, color_blend(*(int *)(tex->data + offset),
-			light.color, demenish_light(hit->wall, light.emittance, bounce - 1)), 1));
+			light.color, FROM_FP(demenish_light(hit->wall, TO_FP(light.emittance), bounce - 1))), 1));
 }
 
 static inline void	draw_floor(t_data *data, t_point curr, t_rdir ray, char *img, t_hit *hit)
@@ -208,14 +238,14 @@ static inline void	draw_floor(t_data *data, t_point curr, t_rdir ray, char *img,
 	light = data->lmap.lmap[(int)(world.x * LMAP_PRECISION)
 		+ (int)(world.y * LMAP_PRECISION) * data->lmap.wid].ce_fl;
 	set_pixels(data, img, get_color(hit, bounce - 1, color_blend(*(int *)(tex->data + offset),
-			light.color, demenish_light(hit->wall, light.emittance, bounce - 1)), 1));
+			light.color, FROM_FP(demenish_light(hit->wall, TO_FP(light.emittance), bounce - 1))), 1));
 }
 
 static inline void	draw_wall(t_data *data, char *img, t_hit *hit)
 {
 	int			color;
 
-	color = get_color(hit, hit->bounces, 0, 0);
+	color = get_color(hit, hit->bounces, (t_col){0}, 0);
 	set_pixels(data, img, color);
 }
 
