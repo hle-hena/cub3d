c64a2130d7873af4459359e8ed3698c39f82f4b4
Gulf1603 <hugoleh@icloud.com>
2025-08-28 15:05:02 +0200

    The rest of the norm is for Gabio

--- srcs/game/draw/raycast/draw_map.c
+++ srcs/game/draw/raycast/draw_map.c
@@ -6,18 +6,11 @@
 /*   By: hle-hena <hle-hena@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/13 22:06:06 by hle-hena          #+#    #+#             */
-/*   Updated: 2025/04/24 12:24:35 by hle-hena         ###   ########.fr       */
+/*   Updated: 2025/08/28 15:03:15 by hle-hena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
-
-	// struct timespec start, end;
-	// clock_gettime(CLOCK_MONOTONIC, &start);
-	// CODE TO BENCHMARK
-	// clock_gettime(CLOCK_MONOTONIC, &end);
-	// long ms = (end.tv_sec - start.tv_sec) * 1000000000L + (end.tv_nsec - start.tv_nsec);
-	// printf("Took %ldns\n", ms);
 
 static inline void	draw_ceil(t_data *data, t_point curr, t_rdir ray, char *img)
 {
@@ -83,7 +76,6 @@ static inline void	draw_floor(t_data *data, t_point curr, t_rdir ray, char *img)
 	*(int *)img = *(int *)(tex->data + offset);
 }
 
-
 void	*draw_walls_thread(void *arg)
 {
 	t_th_draw	*td;
@@ -105,7 +97,8 @@ void	*draw_walls_thread(void *arg)
 			if (curr.y >= hit->draw_start && curr.y < hit->draw_end)
 			{
 				hit->tex_y = hit->tex_pos_fp >> 16;
-				*(int *)img = *(int *)(hit->tex_col + hit->tex_y * hit->texture->size_line);
+				*(int *)img = *(int *)(hit->tex_col + hit->tex_y
+						* hit->texture->size_line);
 				hit->tex_pos_fp += hit->step_fp;
 			}
 			else if (curr.y < hit->draw_start)
@@ -139,8 +132,11 @@ void	draw_walls(t_data *data)
 	{
 		td[i].data = data;
 		td[i].start_x = i * slice;
-		td[i].end_x = (i == DRAW_THREADS - 1) ? data->win_wid : (i + 1) * slice;
-		td[i].add_next_line = (td[i].start_x + data->win_wid - td[i].end_x) * data->img.bpp;
+		td[i].end_x = (i + 1) * slice;
+		if (i == DRAW_THREADS - 1)
+			td[i].end_x = data->win_wid;
+		td[i].add_next_line = (td[i].start_x + data->win_wid - td[i].end_x)
+			* data->img.bpp;
 		td[i].ray_dir = ray_dir;
 		pthread_create(&threads[i], NULL, draw_walls_thread, &td[i]);
 	}
@@ -149,7 +145,7 @@ void	draw_walls(t_data *data)
 		pthread_join(threads[i], NULL);
 }
 
-t_hit cast_ray(t_data *data, t_vec ray_dir)
+t_hit	cast_ray(t_data *data, t_vec ray_dir)
 {
 	t_hit	ray_hit;
 	float	wall_x;
@@ -159,7 +155,8 @@ t_hit cast_ray(t_data *data, t_vec ray_dir)
 
 	ray_hit = raycast(data, ray_dir, data->map->player);
 	ray_hit.ray_dir = ray_dir;
-	ray_hit.dist = ray_hit.dist * (ray_dir.x * data->cam.dir.x + ray_dir.y * data->cam.dir.y);
+	ray_hit.dist = ray_hit.dist * (ray_dir.x * data->cam.dir.x + ray_dir.y
+			* data->cam.dir.y);
 	if (ray_hit.dist <= 0.0f)
 		ray_hit.dist = 0.0001f;
 	line_height = (int)(data->win_len * 2 / ray_hit.dist);
@@ -176,24 +173,20 @@ t_hit cast_ray(t_data *data, t_vec ray_dir)
 	wall_x -= (int)wall_x;
 	line_height = tex_end - tex_start;
 	ray_hit.step_fp = (ray_hit.texture->height << 16) / line_height;
-	ray_hit.tex_pos_fp = (ray_hit.draw_start - data->win_len / 2 + line_height / 2) * ray_hit.step_fp;
-	ray_hit.tex_col = ray_hit.texture->data + (int)(wall_x * ray_hit.texture->width) * ray_hit.texture->bpp;
+	ray_hit.tex_pos_fp = (ray_hit.draw_start - data->win_len / 2 + line_height
+			/ 2) * ray_hit.step_fp;
+	ray_hit.tex_col = ray_hit.texture->data + (int)(wall_x
+			* ray_hit.texture->width) * ray_hit.texture->bpp;
 	return (ray_hit);
 }
 
-void cast_rays(t_data *data)
+void	cast_rays(t_data *data)
 {
 	int		x;
 	float	cam_x;
 	t_vec	ray_dir;
 	float	look_dir;
 
-	int		debug = 0;//////////////////////////set this
-	struct	timespec start, end;
-	long	ms;
-
-	if (debug)
-		clock_gettime(CLOCK_MONOTONIC, &start);
 	look_dir = (float)data->map->player.rot * PI / 180;
 	data->cam.dir = (t_vec){cos(look_dir), sin(look_dir)};
 	data->cam.plane = (t_vec){-sin(look_dir) * tan((float)(60 * PI / 180) / 2),
@@ -206,21 +199,5 @@ void cast_rays(t_data *data)
 		ray_dir.y = data->cam.dir.y + data->cam.plane.y * cam_x;
 		data->hits[x] = cast_ray(data, ray_dir);
 	}
-	if (debug)
-	{
-		clock_gettime(CLOCK_MONOTONIC, &end);
-		long ms = (end.tv_sec - start.tv_sec) * 1000000000L + (end.tv_nsec - start.tv_nsec);
-		printf("Raycast took\t%ldns\n", ms);
-	}
-
-
-	if (debug)
-		clock_gettime(CLOCK_MONOTONIC, &start);
 	draw_walls(data);
-	if (debug)
-	{
-		clock_gettime(CLOCK_MONOTONIC, &end);
-		ms = (end.tv_sec - start.tv_sec) * 1000000000L + (end.tv_nsec - start.tv_nsec);
-		printf("Draw took\t%ldns\n\n", ms);
-	}
 }
