aaab127ce33ca970d7c329b66779864e8836f0c5
Gulf1603 <hugoleh@icloud.com>
2025-08-29 13:12:51 +0200

    fix segfault

--- srcs/game/draw/mini_map/draw_map.c
+++ srcs/game/draw/mini_map/draw_map.c
@@ -6,7 +6,7 @@
 /*   By: hle-hena <hle-hena@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/12 22:36:25 by hle-hena          #+#    #+#             */
-/*   Updated: 2025/08/28 12:38:35 by hle-hena         ###   ########.fr       */
+/*   Updated: 2025/08/29 11:55:55 by hle-hena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,16 +38,16 @@ void	draw_tiles(t_data *data)
 		curr.x = -1;
 		while (++curr.x < data->map->wid)
 		{
-			start.x = (curr.x) * data->map->mini_map_scale;
-			start.y = (curr.y) * data->map->mini_map_scale;
+			start.x = (curr.x) * data->map->mini_map_scale + curr.x;
+			start.y = (curr.y) * data->map->mini_map_scale + curr.y;
+			if (*(data->map->matrix + curr.y * data->map->wid + curr.x)
+				== data->map->void_char)
+				continue ;
 			if (get_tile_dict()[*(data->map->matrix + curr.y * data->map->wid
 					+ curr.x)]->is_wall)
-				draw_tile(data, start, vals, 0xFF0000);
-			else if (*(data->map->matrix + curr.y * data->map->wid + curr.x)
-				!= data->map->void_char)
-				draw_tile(data, start, vals,
-					*(int *)(get_tile_dict()[*(data->map->matrix + curr.y
-							* data->map->wid + curr.x)]->tex_fl->data));
+				draw_tile(data, start, vals, 0x001B2E);
+			else
+				draw_tile(data, start, vals, 0xADB6C4);
 		}
 	}
 }
@@ -56,5 +56,5 @@ void	draw_mini_map(t_data *data)
 {
 	draw_tiles(data);
 	draw_player(data, data->map->mini_map, -PI / 2);
-	draw_circle(data, data->map->mini_map, data->win_len / 8, 0xFFFFFF);
+	draw_circle(data, data->map->mini_map, data->win_len / 8, 0xADB6C4);
 }
--- srcs/game/draw/mini_map/draw_player.c
+++ srcs/game/draw/mini_map/draw_player.c
@@ -6,13 +6,13 @@
 /*   By: hle-hena <hle-hena@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/13 13:28:24 by hle-hena          #+#    #+#             */
-/*   Updated: 2025/04/13 13:36:52 by hle-hena         ###   ########.fr       */
+/*   Updated: 2025/08/29 11:58:04 by hle-hena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-void	draw_player_tip(t_data *data, t_point center, double theta)
+void	draw_player_tip(t_data *data, t_point center, float theta)
 {
 	t_point	tip;
 	t_point	base;
@@ -23,21 +23,21 @@ void	draw_player_tip(t_data *data, t_point center, double theta)
 		* cos(theta + PI / 2 / 2);
 	base.y = center.y + data->map->mini_map_scale / 4
 		* sin(theta + PI / 2 / 2);
-	draw_line(data, tip, base, 0x00FF00);
+	draw_line(data, tip, base, 0x685044);
 	base.x = center.x + data->map->mini_map_scale / 4
 		* cos(theta - PI / 2 / 2);
 	base.y = center.y + data->map->mini_map_scale / 4
 		* sin(theta - PI / 2 / 2);
-	draw_line(data, tip, base, 0x00FF00);
+	draw_line(data, tip, base, 0x685044);
 }
 
 void	draw_player(t_data *data, t_point center, double theta)
 {
 	t_point	draw;
 	int		i;
-	double	a;
-	double	a_step;
-	double	da;
+	float	a;
+	float	a_step;
+	float	da;
 
 	draw_player_tip(data, center, theta);
 	a_step = 2 * PI / 100;
@@ -50,6 +50,6 @@ void	draw_player(t_data *data, t_point center, double theta)
 			continue ;
 		draw.x = center.x + (int)(data->map->mini_map_scale / 4 * cos(a));
 		draw.y = center.y + (int)(data->map->mini_map_scale / 4 * sin(a));
-		ft_put_pixel(data, draw, 0x00FF00);
+		ft_put_pixel(data, draw, 0x685044);
 	}
 }
